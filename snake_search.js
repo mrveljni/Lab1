06fb5f1dfa3b7db678494aa74221cdc74ff577ce
mrveljni <nikimrvelj@gmail.com>
2015-10-07 14:57:03 -0400

    Snake Search
    Snake Search for course project

---
+++ snake_search.js
@@ -0,0 +1,7 @@
+$('#form1').on('Submit', function()
+               {
+               var queryline = document.getElementById("queryline").value
+               alert('Query line:');
+               alert(queryline)
+               });
+
