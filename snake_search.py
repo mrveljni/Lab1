715e324b2cadbde48f475c944000d70c10fd9843
mrveljni <nikimrvelj@gmail.com>
2015-10-09 18:06:09 -0400

    SNAKE_SEARCH COMPLETE
    Snake_search achievements: check the texts I sent you Matt.

--- snake_search.py
+++ snake_search.py
@@ -1,65 +1,76 @@
 from bottle import *
 from collections import OrderedDict
+from operator import itemgetter
+
+counter = 0
+mainword=""
+maintablestring=""
+maindict=OrderedDict()
 
 @route('/', method="GET")
 
 def main():
+    global counter
+    counter = counter + 1
+    print "This is counter:", counter
+    
     print "You are now in main"
-    word="" #declaring word string
-    tablestring=""
-    dict=OrderedDict() #declaring ordered dictionary datastructure
-    queryresult = request.query['keywords'].lower() #requesting 'keywords' from HTML and making it lowercase
-    querylist = queryresult.split(" ")#splitting queryresult by the spaces and reversing it
-    print querylist
 
-    for word in querylist:
-        if word not in dict:
-            dict[word]=1
+    global mainword #declaring word string
+    global maintablestring
+    global maindict #declaring ordered dictionary datastructure
+    mainqueryresult = request.query['keywords'].lower() #requesting 'keywords' from HTML and making it lowercase
+    mainquerylist = mainqueryresult.split(" ") #splitting queryresult by the spaces and reversing it
+    
+    for mainword in mainquerylist:
+        if mainword not in maindict:
+            maindict[mainword]=1
         else:
-            dict[word] +=1
+            maindict[mainword] +=1
 
-    print "this is main dict:"
-    print dict
-    searchstring = "Search for <i>\"{querystring}\" </i> ".format(querystring=request.query['keywords'])
-    tableheader = "<tr> <td> <b> Word </b> </td> <td> <b> Count </b> </td></tr>"
-    tablebeginning = "<table id = \"results\">"
-    tableend = "</table>"
+    mainsearchstring = "Search for <i>\"{querystring}\" </i> ".format(querystring=request.query['keywords'])
+    maintableheader = "<tr> <td> <b> Word </b> </td> <td> <b> Count </b> </td></tr>"
+    maintablebeginning = "<table id = \"results\">"
+    maintableend = "</table>"
 
-    for k, v in dict.iteritems():
-        print "entered the iteration for loop"
-        tablestring = tablestring + "<tr> <td> {queryword} </td> <td> {querycount} </td>".format(queryword=k, querycount=v)
-    resultstring = searchstring + tablebeginning + tableheader + tablestring + tableend
+    for k, v in maindict.iteritems():
+        maintablestring = maintablestring + "<tr> <td> {queryword} </td> <td> {querycount} </td>".format(queryword=k, querycount=v)
+    mainresultstring = mainsearchstring + maintablebeginning + maintableheader + maintablestring + maintableend
 
-    results()
-
+    return results()
 
 def results():
-    print "You are now in results"
+    i=0
+    global maindict
     word="" #declaring word string
     tablestring=""
     dict=OrderedDict() #declaring ordered dictionary datastructure
+    newdict=OrderedDict()
     queryresult = request.query['keywords'].lower() #requesting 'keywords' from HTML and making it lowercase
     querylist = queryresult.split(" ")#splitting queryresult by the spaces and reversing it
     print querylist
-    
     for word in querylist:
-        if word not in dict:
+        if word in maindict:
+            dict[word]=maindict[word]
+        elif word not in dict:
             dict[word]=1
         else:
             dict[word] +=1
-
     print "this is results dict"
     print dict
     searchstring = "Search for <i>\"{querystring}\" </i> ".format(querystring=request.query['keywords'])
     tableheader = "<tr> <td> <b> Word </b> </td> <td> <b> Count </b> </td></tr>"
     tablebeginning = "<table id = \"results\">"
     tableend = "</table>"
-    
-    for k, v in dict.iteritems():
-        print "entered the iteration for loop"
-        tablestring = tablestring + "<tr> <td> {queryword} </td> <td> {querycount} </td>".format(queryword=k, querycount=v)
+    newdict = sorted(dict, key=dict.get, reverse=True) #returns new dict list of values...not dictionary
+    newdict = newdict[:20]
+    print "This is new dict:", newdict
+    for newdictword in newdict:
+        tablestring = tablestring + "<tr> <td> {queryword} </td> <td> {querycount} </td>".format(queryword=newdictword, querycount=dict[newdictword])
     resultstring = searchstring + tablebeginning + tableheader + tablestring + tableend
+        #for k, v in dict.iteritems():
+            #tablestring = tablestring + "<tr> <td> {queryword} </td> <td> {querycount} </td>".format(queryword=k, querycount=v)
+        #resultstring = searchstring + tablebeginning + tableheader + tablestring + tableend
     return resultstring;
-
 
 run(hosts='localhost', port=8080, debug=True)
